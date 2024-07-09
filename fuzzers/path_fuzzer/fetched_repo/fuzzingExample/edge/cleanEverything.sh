b85cc423dbc7b18f91ec096c21ab891674586d3a
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-07-09 16:58:20 +0800

    add path_fuzzer dir

---
+++ fuzzers/path_fuzzer/fetched_repo/fuzzingExample/edge/cleanEverything.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+# 运行之前记得切换到 path_fuzzer_with_reduction 分支 
+
+rm ./bbid.txt ./callmap.txt ./cfg.txt ./function_list.txt ./bbnum.txt ./convert 
+rm ./cfg_filtered.txt ./callmap_filtered.txt
+rm ./function_list.txt
+rm ./convert
+rm ./edge_cfg.bin
+rm ./edge ./libpath_reduction.so
+rm -rf output_dir
+rm ./edge_cmplog
+
+
+
+
