34b89b1e387fd32a8905e85af597d0ccad117f65
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-07-10 01:52:58 +0800

    ready to experiment: see whether 12 PUTs can run

--- fuzzers/path_fuzzer/instrument.sh
+++ fuzzers/path_fuzzer/instrument.sh
@@ -24,9 +24,29 @@ export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
     export OUT="$OUT/afl"
     export LDFLAGS="$LDFLAGS -L$OUT"
 
+    # $MAGMA/build.sh 所描述的是 MAGMA 日志协作代码，不需要在这上面插桩
     "$MAGMA/build.sh"
+
+    # $TARGET/build.sh 所描述的是 PUT 的构建代码，需要插桩
+    # 1. 删除 $OUT/ 里的 "残余" 内容
+    rm $OUT/bbid.txt $OUT/callmap.txt $OUT/cfg.txt $OUT/function_list.txt $OUT/bbnum.txt $OUT/convert
+    # 2. 设置好环境变量，进行插桩
+    export BBIDFILE="$OUT/bbid.txt"
+    export CALLMAPFILE="$OUT/callmap.txt"
+    export CFGFILE="$OUT/cfg.txt"
+    export LD_LIBRARY_PATH=$FUZZER
+    export AFL_LLVM_CALLER=1
     "$TARGET/build.sh"
+     
+    # 在 $FUZZER/run.sh 中实现 3. 和 4. (分别是对 cfg.txt 和 callmap.txt 过滤，以及生成 CFG binary)
 )
+
+# !/bin/bash
+# 运行之前记得切换到 path_fuzzer_with_reduction 分支
+
+
+
+
 
 # # Build the CmpLog instrumented version
 # 
--- fuzzers/path_fuzzer/run.sh
+++ fuzzers/path_fuzzer/run.sh
@@ -11,6 +11,17 @@
 # - env FUZZARGS: extra arguments to pass to the fuzzer
 ##
 
+# 3. 过滤掉 cfg.txt 和 callmap.txt 的东西，以及生成一些别的文本
+bash $FUZZER/filterCFG_Callmap_script.sh
+cat $OUT/cfg.txt | grep "BasicBlock: " | wc -l > $OUT/bbnum.txt
+cat $OUT/cfg_filtered.txt | grep "Function: " | nl -v 0 | awk '{print $1, $3, $4, $5, $6, $7, $8, $9}' > $OUT/function_list.txt
+
+# 4. 使用之前构建的东西，生成 CFG binary
+g++ $FUZZER/convert.cpp -o $OUT/convert
+$OUT/convert
+mv $OUT/top.bin $OUT/${PROGRAM}_cfg.bin
+
+# run.sh 本来的内容
 if nm "$OUT/afl/$PROGRAM" | grep -E '^[0-9a-f]+\s+[Ww]\s+main$'; then
     ARGS="-"
 fi
@@ -24,6 +35,11 @@ export AFL_NO_AFFINITY=1
 export AFL_NO_UI=1
 export AFL_MAP_SIZE=256000
 # export AFL_DRIVER_DONT_DEFER=1
+
+# 针对 path_fuzzer 的一些环境变量设置
+export CFG_BIN_FILE="$OUT/${PROGRAM}_cfg.bin"
+export LD_LIBRARY_PATH=$FUZZER
+export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
 
 set -x
 
