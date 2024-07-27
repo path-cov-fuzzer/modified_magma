9742dbb9f964c462209c002f66f5c0df2c616e6d
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-07-27 22:16:53 +0800

    pass sqlite3 successfully

--- targets/sqlite3/fetch.sh
+++ targets/sqlite3/fetch.sh
@@ -5,7 +5,9 @@
 # - env TARGET: path to target work dir
 ##
 
-curl "https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=8c432642572c8c4b" \
-  -o "$OUT/sqlite.tar.gz" && \
+# curl "https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=8c432642572c8c4b" \
+  # -o "$OUT/sqlite.tar.gz" && \
+
 mkdir -p "$TARGET/repo" && \
-tar -C "$TARGET/repo" --strip-components=1 -xzf "$OUT/sqlite.tar.gz"
+tar -C "$TARGET/repo" --strip-components=1 -xzf "$TARGET/sqlite.tar.gz"
+
