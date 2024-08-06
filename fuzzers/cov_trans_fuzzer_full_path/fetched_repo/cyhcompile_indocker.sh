127e816be9c3c1bc33dcf665d7fb13d9feddb602
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-08-06 23:43:39 +0800

    fix some bugs

--- fuzzers/cov_trans_fuzzer_full_path/fetched_repo/cyhcompile_indocker.sh
+++ fuzzers/cov_trans_fuzzer_full_path/fetched_repo/cyhcompile_indocker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-make clean
-
-bash update_path_reduction.sh
+# bash update_path_reduction.sh
 
 g++ -c hashcompare.cpp
 ar rcs libhashcompare.a hashcompare.o
@@ -15,22 +13,5 @@ export CXX=clang++
 export AFL_NO_X86=1
 export PYTHON_INCLUDE=/
 LLVM_CONFIG=llvm-config-17 LD_LIBRARY_PATH=$(pwd) make -e -C utils/aflpp_driver || exit 1
-
-
-# #!/bin/bash
-
-# # bash update_path_reduction.sh
-
-# g++ -c hashcompare.cpp
-# ar rcs libhashcompare.a hashcompare.o
-# export AFL_NO_X86=1
-# export PYTHON_INCLUDE=/
-# LLVM_CONFIG=llvm-config-17 LD_LIBRARY_PATH=$(pwd) CFLAGS="-I$(pwd)" LDFLAGS="-L$(pwd) -lcrypto -lhashcompare -lstdc++ -lpath_reduction" make -e -j$(nproc)
-
-# export CC=clang
-# export CXX=clang++
-# export AFL_NO_X86=1
-# export PYTHON_INCLUDE=/
-# LLVM_CONFIG=llvm-config-17 LD_LIBRARY_PATH=$(pwd) make -e -C utils/aflpp_driver || exit 1
 
 
