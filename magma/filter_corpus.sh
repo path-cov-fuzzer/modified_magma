9ec0d50a5971c66cdad9dbf81e566131b79d728f
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-08-19 08:10:19 +0800

    fix bugs

--- magma/filter_corpus.sh
+++ magma/filter_corpus.sh
@@ -6,9 +6,8 @@ for PROGRAM in "${PROGRAMS[@]}"
 do
     # prune the seed corpus for any fault-triggering test-cases
     for seed in "$TARGET/corpus/$PROGRAM"/*; do
-        echo "seed = $seed" 
+
         out="$("$MAGMA"/runonce.sh "$seed")"
-        echo 
         code=$?
 
         if [ $code -ne 0 ]; then
