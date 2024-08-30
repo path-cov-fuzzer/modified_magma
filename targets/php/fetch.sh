ec5009acf2e4192b446117b2d8f0573c31d04a60
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-08-31 03:12:56 +0800

    change php's fetch.sh

--- targets/php/fetch.sh
+++ targets/php/fetch.sh
@@ -5,10 +5,12 @@
 # - env TARGET: path to target work dir
 ##
 
-git clone --no-checkout https://github.com/php/php-src.git \
-    "$TARGET/repo"
-git -C "$TARGET/repo" checkout bc39abe8c3c492e29bc5d60ca58442040bbf063b
+mv $TARGET/fetched_repo $TARGET/repo
 
-git clone --no-checkout https://github.com/kkos/oniguruma.git \
-    "$TARGET/repo/oniguruma"
-git -C "$TARGET/repo/oniguruma" checkout 227ec0bd690207812793c09ad70024707c405376
+# git clone --no-checkout https://github.com/php/php-src.git \
+    # "$TARGET/repo"
+# git -C "$TARGET/repo" checkout bc39abe8c3c492e29bc5d60ca58442040bbf063b
+
+# git clone --no-checkout https://github.com/kkos/oniguruma.git \
+    # "$TARGET/repo/oniguruma"
+# git -C "$TARGET/repo/oniguruma" checkout 227ec0bd690207812793c09ad70024707c405376
