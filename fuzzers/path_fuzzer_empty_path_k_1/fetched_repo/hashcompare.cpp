7c8be1ac377ccee88877d8ba90db450cd3ac8bba
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-08-31 01:06:26 +0800

    migrate empty libpath_reduction.so, waiting for test

--- fuzzers/path_fuzzer_empty_path_k_1/fetched_repo/hashcompare.cpp
+++ fuzzers/path_fuzzer_empty_path_k_1/fetched_repo/hashcompare.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <openssl/sha.h>
 #include <unordered_map>
+#include <cassert>
+#include <cstdint>
 
 std::unordered_map<std::string, bool> hashMap;
+// std::unordered_map<std::string, std::string> hashMap;
 
 extern "C" {
 
+    // bool hashcompare(unsigned char trace_hash[SHA256_DIGEST_LENGTH], bool crash_if_collide);
     bool hashcompare(unsigned char trace_hash[SHA256_DIGEST_LENGTH]);
 
+    // bool hashcompare(unsigned char trace_hash[SHA256_DIGEST_LENGTH], bool crash_if_collide) {
     bool hashcompare(unsigned char trace_hash[SHA256_DIGEST_LENGTH]) {
 
         bool interesting = false;
@@ -20,6 +25,7 @@ extern "C" {
           int written_bytes = snprintf((&hash[0]) + total_len, 2 * SHA256_DIGEST_LENGTH + 1 - total_len, "%02x", trace_hash[i]);
           total_len += written_bytes;
         }
+        hash[2 * SHA256_DIGEST_LENGTH] = '\0';
 
         std::string hashStr(hash); // 使用char数组初始化字符串
 
