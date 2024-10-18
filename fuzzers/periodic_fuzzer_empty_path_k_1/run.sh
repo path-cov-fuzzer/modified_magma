580003ee3f7a90960a71e8e1dd62b061fbd85943
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-10-19 01:00:03 +0800

    change periodic_fuzzer_empty_path_k_1 run.sh

--- fuzzers/periodic_fuzzer_empty_path_k_1/run.sh
+++ fuzzers/periodic_fuzzer_empty_path_k_1/run.sh
@@ -49,16 +49,64 @@ export PATH_REDUCTION_ON_ERROR="EMPTY_PATH"
 
 cp $OUT/cfg.txt $SHARED/cfg.txt
 
-set -x
-
 mkdir -p $SHARED/afl
 mkdir -p $SHARED/cmplog
 cp  $OUT/afl/$PROGRAM $SHARED/afl/$PROGRAM
 cp  $OUT/cmplog/$PROGRAM $SHARED/cmplog/$PROGRAM
 
+# ===============================================================
+interval=60
+output="$SHARED/findings"
+
 "$FUZZER/repo/afl-fuzz" -i "$SHARED/corpus/$PROGRAM" -o "$SHARED/findings" \
     "${flag_cmplog[@]}" -d \
-    $FUZZARGS -- "$OUT/afl/$PROGRAM" $ARGS 2>&1
+    $FUZZARGS -- "$OUT/afl/$PROGRAM" $ARGS 2>&1 &
+pid=$!
+elapsed_time=0
 
-set +x
+while true
+do
+    # 每隔 interval 执行一次循环体
+    sleep $interval
+    elapsed_time=$(( elapsed_time + interval ))
+    echo "Running command at $(date)"
+    # 如果 +cov 文件相比之前增加了，那么 kill 掉 afl-fuzz，随后删掉所有 +pat 种子，再重启
+    cov_count=$(find "$output/default/queue" -type f -name '*+cov*' | wc -l)
+    echo "cov_count = $cov_count"
+    if [ "$cov_count" -gt 0 ]; then
+        kill -9 $pid
+	# 拷贝 plot_data 数据，加上时间命名
+	# cp "$output/default/plot_data" "$plot_data_dir/$elapsed_time"
+	# 使用 find 查找文件名包含 '+pat' 的文件并删除
+        find "$output/default/queue" -type f -name '*+pat*' -exec rm -f {} \;
+	# 整理 crashes 文件夹
+	mv "$output/default/crashes" "$SHARED/crashes.$elapsed_time"
+	# 把剩余的种子作为新一轮的起始种子
+	mv "$output/default/queue" "$SHARED/corpus.$elapsed_time"
+	# 把所有种子重命名一遍
+	counter=1
+        for file in "$SHARED/corpus.$elapsed_time"/*; do
+        # 如果是文件才重命名
+            if [ -f "$file" ]; then
+                # 生成新的文件名
+                new_name=$(printf "a%04d" "$counter")
+                # 生成新的文件路径
+                new_path="$SHARED/corpus.$elapsed_time/$new_name"
+                # 重命名文件
+                mv "$file" "$new_path"
+                # 增加计数器
+                counter=$((counter + 1))
+            fi
+        done
+
+	# 重启命令
+	rm -rf $output
+	"$FUZZER/repo/afl-fuzz" -i "$SHARED/corpus.$elapsed_time" -o "$SHARED/findings" \
+    		"${flag_cmplog[@]}" -d \
+    		$FUZZARGS -- "$OUT/afl/$PROGRAM" $ARGS 2>&1 &
+        pid=$!
+    fi
+done
+
+
 
