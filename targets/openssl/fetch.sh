0aeb554a387008aa16df689d2b02c6b870cb7152
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-10-26 19:00:45 +0800

    change openssl src

--- targets/openssl/fetch.sh
+++ targets/openssl/fetch.sh
@@ -5,8 +5,10 @@
 # - env TARGET: path to target work dir
 ##
 
-git clone --no-checkout https://github.com/openssl/openssl.git \
-    "$TARGET/repo"
-git -C "$TARGET/repo" checkout 3bd5319b5d0df9ecf05c8baba2c401ad8e3ba130
+# git clone --no-checkout https://github.com/openssl/openssl.git \
+    # "$TARGET/repo"
+# git -C "$TARGET/repo" checkout 3bd5319b5d0df9ecf05c8baba2c401ad8e3ba130
+
+mv "$TARGET/fetched_repo" "$TARGET/repo"
 
 cp "$TARGET/src/abilist.txt" "$TARGET/repo/abilist.txt"
